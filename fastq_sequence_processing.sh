84c3c375e6fc9786ecffd3cbd4993e9fee974c98
Allison Bogisich <asbogisich@dons.usfca.edu>
2017-10-03 23:46:55 +0000

    Add script for file download and QC reports

---
+++ fastq_sequence_processing.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Script to process NCBI fastq sequences to then use for a BLAST search within GenBank
+
+# Allison Bogisich, asbogisich@dons.usfca.edu
+# October 2, 2017
+
+
+# Check to see if there is a value for variable BLASTDB, and sets it if there is not
+if [ -z ${BLASTDB} ]; then source /home/.bashrc; export PATH; export BLASTDB; fi
+
+# Download the list of files in the run table to the raw data directory
+# the pipe and tail -n +2 is a nice way to exclude the first line
+for SRA_number in $(cut -f 6 data/metadata/fierer_forensic_hand_mouse_SraRunTable.txt | tail -n +2)
+do
+	fastq-dump -v $SRA_number -O data/raw_data
+done
+echo "List of files downloaded."
+
+
+#Create QC reports for each of the runs using FASTQC program
+echo "Create QC reports for each of the data runs."
+echo
+fastqc data/raw_data/*.fastq --outdir=output/fastqc
+echo
+echo "QC reports made."
